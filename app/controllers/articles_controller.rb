aaaa473c00e34ed028e779c21a0d702755896bad
路小鹿 <1551755561@qq.com>
2022-06-17 12:47:15 +0800

    stash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,27 @@
 class ArticlesController < ApplicationController
-    def new
-        puts "new article"
+    def index
+        @articles = Article.all
     end
 
     def create
         print 'params=', params, "\n"
-        render plain: params[:article].inspect
+        # @article = Article.new(params[:article])
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to @article
     end
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
+
 end
